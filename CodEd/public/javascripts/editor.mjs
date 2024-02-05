e55271b582db7646ce12976e5eb71f96e3dbf68b
Kendellb <92357397+Kendellb@users.noreply.github.com>
2024-02-05 12:57:21 -0500

    Added jsdoc for the editor.mjs file

--- CodEd/public/javascripts/editor.mjs
+++ CodEd/public/javascripts/editor.mjs
@@ -1,3 +1,12 @@
+/**
+ * Creates and initializes a CodeMirror editor using the basic setup. using the config from CodeMirror Docs.
+ *
+ * @class
+ * @constructor
+ * @param {Object} options - Configuration options for the EditorView.
+ * @param {EditorState} options.state - The initial state of the editor.
+ * @param {HTMLElement} options.parent - The DOM element to which the editor should be appended.
+ */
 import { EditorState, EditorView, basicSetup } from "@codemirror/basic-setup";
 //import { javascript } from "@codemirror/lang-javascript";
 
