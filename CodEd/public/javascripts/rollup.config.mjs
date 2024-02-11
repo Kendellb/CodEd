c85e01e34e6dfa1df1cd5c65c98937ce85d7d568
Kendellb <92357397+Kendellb@users.noreply.github.com>
2024-02-11 17:35:58 -0500

    Added user acceptace test and docuentation

--- CodEd/public/javascripts/rollup.config.mjs
+++ CodEd/public/javascripts/rollup.config.mjs
@@ -1,3 +1,13 @@
+/**
+ * Configuration object for Rollup bundling.
+ * 
+ * @type {Object}
+ * @property {string} input - The entry point for the bundling process.
+ * @property {Object} output - The configuration for the output bundle.
+ * @property {string} output.file - The file path for the output bundle.
+ * @property {string} output.format - The format of the output bundle (CommonJS in this case).
+ * @property {Array<Plugin>} plugins - An array of Rollup plugins to use during bundling.
+ */
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 export default {
   input: "./editor.mjs",
