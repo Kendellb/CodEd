5e54e5324f8cf87d3dd948364aaf5bb92fb393f5
Kendellb <92357397+Kendellb@users.noreply.github.com>
2024-03-18 13:27:56 -0400

    bat works now

--- CodEd/jsdoc.js
+++ CodEd/jsdoc.js
@@ -6,13 +6,10 @@ const os = require('os');
 function runJSDocCommand() {
     const isWindows = os.platform() === 'win32';
 
-    // Define the path to the script based on the OS
     const scriptPath = isWindows ? 'generate_docs.bat' : './generate_docs.sh';
 
-    // Get the absolute path to the script
     const absoluteScriptPath = path.resolve(__dirname, scriptPath);
 
-    // Execute the command
     exec(absoluteScriptPath, (error, stdout, stderr) => {
         if (error) {
             console.error(`Error executing command: ${error}`);
@@ -25,6 +22,5 @@ function runJSDocCommand() {
     });
 }
 
-// Run JSDoc command
 runJSDocCommand();
 
